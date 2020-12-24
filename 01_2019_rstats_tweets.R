fe04229e8128054d26b2ece289fe59653493aa48
Felix Schier <felix.schier@t-online.de>
2020-12-24 15:35:16 +0100

    Commit_1

---
+++ 01_2019_rstats_tweets.R
@@ -0,0 +1,12 @@
+library(tidytuesdayR)
+library(tidyverse)
+
+
+
+# Tidy Tuesday 01-2019 - rstats tweets ------------------------------------
+
+
+
+data <- tt_load(x = 2019, week = 1)
+
+data$tidytuesday_tweets
